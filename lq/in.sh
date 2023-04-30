363b6a3fd20ca49af5a52c63e0fec356f3192d3b
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-04-30 09:29:54 +0000

    🐟

---
+++ lq/in.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/liqowomo/lq-gudShFish-1/main/lq/fixrs.fish | fish 
+curl https://raw.githubusercontent.com/liqowomo/lq-gudShFish-1/main/lq/in.sh | bash
+curl https://raw.githubusercontent.com/liqowomo/lq-gudShFish-1/main/lq/st.sh | bash 
